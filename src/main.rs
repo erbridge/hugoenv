7c569bc4b5c24da1fea126aa004478c069fd3d78
erbridge <f@erbridge.co.uk>
2022-10-09 23:49:13 +0200

    Create a root directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use std::env;
+use std::path::Path;
+
+const ROOT_ENV_VAR_NAME: &str = "HUGOENV_ROOT";
+const ROOT_DEFAULT: &str = "~/.hugoenv";
 
 /// Manage Hugo versions
 #[derive(Parser)]
@@ -18,11 +23,23 @@ enum Command {
 fn main() -> Result<()> {
     let args = Cli::parse();
     match args.command {
-        Command::Install { version } => install(version),
+        Command::Install { version } => install(version)?,
     }
     Ok(())
 }
 
-fn install(version: std::string::String) {
-    println!("Installing {}...", version)
+fn install(version: std::string::String) -> Result<()> {
+    println!("Installing {version}...");
+    ensure_root_dir()?;
+    Ok(())
+}
+
+fn ensure_root_dir() -> Result<()> {
+    let dir_string = env::var(ROOT_ENV_VAR_NAME).unwrap_or_else(|_| String::from(ROOT_DEFAULT));
+    let expanded_dir_string = shellexpand::tilde(&dir_string);
+    let dir = Path::new(expanded_dir_string.as_ref());
+    if !dir.exists() {
+        std::fs::create_dir_all(dir)?;
+    }
+    Ok(())
 }
